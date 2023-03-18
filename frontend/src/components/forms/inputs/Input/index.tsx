cb1a6207372de8130c9e7300812418fdd6c2b3aa
Jaume Castelló <jaume97.cs@gmail.com>
2023-03-19 00:59:54 +0100

    [FRONT] Fix Input ref console error

--- frontend/src/components/forms/inputs/Input/index.tsx
+++ frontend/src/components/forms/inputs/Input/index.tsx
@@ -1,30 +1,41 @@
-import type { FC } from "react";
-import { useId } from "react";
+import { forwardRef, useId } from "react";
 import { useController, useFormContext } from "react-hook-form";
 import * as S from "./styled";
 import type { InputBaseProps, InputProps } from "./types";
 
-export const InputBase: FC<InputBaseProps> = ({ label = "", ...rest }) => {
-  const id = useId();
+export const InputBase = forwardRef<HTMLInputElement, InputBaseProps>(
+	({ label = "", ...rest }, ref) => {
+		const id = useId();
 
-  return (
-    <S.InputWrapper>
-      <S.InputLabel htmlFor={rest.id || id}>{label}</S.InputLabel>
-      <S.InputElement {...rest} id={rest.id || id} />
-    </S.InputWrapper>
-  );
-};
+		return (
+			<S.InputWrapper>
+				<S.InputLabel htmlFor={rest.id || id}>{label}</S.InputLabel>
+				<S.InputElement {...rest} id={rest.id || id} ref={ref} />
+			</S.InputWrapper>
+		);
+	}
+);
+InputBase.displayName = "InputBase";
 
-const Input: FC<InputProps> = ({ rules, name, ...rest }) => {
-  const { control } = useFormContext();
-  const { field } = useController({
-    name,
-    control,
-    rules,
-    defaultValue: rest.value,
-  });
+const Input = forwardRef<HTMLInputElement, InputProps>(
+	({ rules, name, ...rest }, ref) => {
+		const { control } = useFormContext();
+		const { field } = useController({
+			name,
+			control,
+			rules,
+			defaultValue: rest.value || "",
+		});
+		const handleRef = (inputRef: HTMLInputElement) => {
+			field.ref(inputRef);
+			if (typeof ref === "function") {
+				ref(inputRef);
+			}
+		};
 
-  return <InputBase {...rest} {...field} />;
-};
+		return <InputBase {...rest} {...field} ref={handleRef} />;
+	}
+);
+Input.displayName = "Input";
 
 export default Input;
