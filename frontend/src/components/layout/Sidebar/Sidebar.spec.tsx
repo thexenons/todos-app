ca5b30d2c304fd8d209beb92198fed85265dd43d
Jaume Castelló <jcastello@ext.laliga.es>
2023-04-28 17:27:59 +0200

    [FRONT] Component - Sidebar (improve tests)

--- frontend/src/components/layout/Sidebar/Sidebar.spec.tsx
+++ frontend/src/components/layout/Sidebar/Sidebar.spec.tsx
@@ -1,7 +1,14 @@
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
+import { useElementSize } from "usehooks-ts";
+import { vi } from "vitest";
 
 import BaseTestComponent from "../../../tests/BaseTestComponent";
 import Sidebar from ".";
+
+vi.mock("usehooks-ts");
+vi.mocked(useElementSize, {
+	partial: true,
+}).mockReturnValue([vi.fn(), { width: 100, height: 100 }]);
 
 describe("Sidebar", () => {
 	it("Render", () => {
@@ -11,5 +18,37 @@ describe("Sidebar", () => {
 			</BaseTestComponent>
 		);
 		expect(container).toMatchSnapshot();
+	});
+
+	it("Force isOpen true", () => {
+		const { container } = render(
+			<BaseTestComponent>
+				<Sidebar isSidebarOpen={true} />
+			</BaseTestComponent>
+		);
+
+		expect(container).toMatchSnapshot();
+	});
+
+	it("Force isOpen false", () => {
+		const { container } = render(
+			<BaseTestComponent>
+				<Sidebar isSidebarOpen={false} />
+			</BaseTestComponent>
+		);
+
+		expect(container).toMatchSnapshot();
+	});
+
+	it("must render children", async () => {
+		render(
+			<BaseTestComponent>
+				<Sidebar>
+					<div data-testid="SidebarChildren" />
+				</Sidebar>
+			</BaseTestComponent>
+		);
+
+		expect(screen.getByTestId("SidebarChildren")).toBeInTheDocument();
 	});
 });
